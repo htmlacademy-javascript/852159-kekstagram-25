779c7c5d9a7f5dd2d1ead924deb4b5fe885077a5
Kate Sm <ktushsmit@gmail.com>
2022-03-30 11:06:44 +0200

    правочки 2 задание

--- js/main.js
+++ js/main.js
@@ -3,24 +3,22 @@ const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   if(min < 0) {
-    min = Math.abs(min);
+    return null;
   }
   if(max < 0) {
-    max = Math.abs(max);
+    return null;
   }
   if (max === min) {
     return max;
   }
   if (max < min ) {
-    const T = max;
-    max = min;
-    min = T;
+    return null;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-getRandomInt ();
+getRandomInt (1, 10);
 
 // Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна
-const getLength = (stringChecked, maxLength) => stringChecked.length <= maxLength;
-getLength('Проверка', 24);
+const checkStringLength = (stringChecked, maxLength) => stringChecked.length <= maxLength;
+checkStringLength('Проверка', 24);
