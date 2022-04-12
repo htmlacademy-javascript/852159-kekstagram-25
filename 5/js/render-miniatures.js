fe0779e55b4dffb1db39795f739980c0c50d30bf
Keks <keks@htmlacademy.ru>
2022-04-12 12:01:47 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/render-miniatures.js
@@ -0,0 +1,27 @@
+// Функция для отрисовки миниатюр
+const renderMiniPictures = (postsArray) => {
+  // Нода, в которую мы добавим результат
+  const pictures = document.querySelector('.pictures');
+  // Шаблон, по которому будем создавать фотографии
+  const pictureTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+  // Фрагмент DOM дерева, который мы будем наполнять в соответсвии с шаблоном
+  const picturesFragment = document.createDocumentFragment();
+
+  postsArray.forEach((post) => {
+    // Создаем новую ноду по темплейту
+    const photo = pictureTemplate.cloneNode(true);
+    // Заполняем значениями
+    photo.querySelector('.picture__img').src = post.url;
+    photo.querySelector('.picture__comments').textContent =
+      post.comments.length;
+    photo.querySelector('.picture__likes').textContent = post.likes;
+
+    picturesFragment.appendChild(photo);
+  });
+  // Добавляем результат в DOM
+  pictures.appendChild(picturesFragment);
+};
+
+export { renderMiniPictures };
---
+++ 5/js/util.js
@@ -0,0 +1,41 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+function getRandomPositiveInteger (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+// Функция возвращающая генератор уникальных случайных целых чисел в диаапозоне min, max.
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+// Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария, но должна быть универсальна
+const checkStringLength = (stringChecked, maxLength) => stringChecked.length <= maxLength;
+
+// Функция для получения случайного элемента из массива. Принимает на вход массив, из которого следует выбрать элемент.
+const getRandomArrayElement = (array) => array[getRandomPositiveInteger(0, array.length - 1)];
+
+// Функция возвращающая генератор уникальных идентефикаторов.
+function createIdGenerator() {
+  let lastGeneratedId = 0;
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+export {getRandomPositiveInteger, checkStringLength, createRandomIdFromRangeGenerator, getRandomArrayElement, createIdGenerator};
